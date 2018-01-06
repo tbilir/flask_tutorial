ae6fbb26bcfbab7b0e4a2331b7bea75fa19e49bd
Taner Bilir <tbilir@SolarJunction.local>
2018-01-05 17:17:18 -0800

    Lesson 2 Commit

--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -5,9 +5,21 @@ Created on Thu Jan  4 17:06:27 2018
 @author: tbilir
 """
 
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'username': 'Miguel'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
