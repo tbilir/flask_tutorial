b48aba31cf47da185cbd97f6d2f7d00cf8fed11a
Taner Bilir <tbilir@sj-solar.com>
2018-01-12 17:15:25 -0800

    Lesson 3 Commit

--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -5,7 +5,7 @@ Created on Thu Jan  4 17:06:27 2018
 @author: tbilir
 """
 
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
 from app import app
 from app.forms import LoginForm
 
@@ -25,7 +25,11 @@ def index():
     ]
     return render_template('index.html', title='Home', user=user, posts=posts)
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect(url_for('index'))
     return render_template('login.html', title='Sign In', form=form)
